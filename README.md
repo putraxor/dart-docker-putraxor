56913fe8aea36f7ebc39cd47bee7af1c8f7bef74
Putra <putraxor@gmail.com>
2019-01-24 16:39:08 +0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dart-docker-putraxor
 Dart docker runtime
+
+Build and push image:
+```
+docker build -t putraxor/dart_putraxor -f Dockerfile .
+docker push putraxor/dart_putraxor:latest
+```
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DBG_OPTION=
+# Only enable Dart debugger if DBG_ENABLE is set.
+if [ -n "$DBG_ENABLE" ] && [ "$GAE_PARTITION" = "dev" ]; then
+  echo "Enabling Dart debugger"
+  DBG_OPTION="--debug:${DBG_PORT:-5858}/0.0.0.0"
+  echo "Starting Dart with additional options $DBG_OPTION"
+fi
+
+if [ -n "$DART_VM_OPTIONS" ]; then
+  echo "Starting Dart with additional options $DART_VM_OPTIONS"
+fi
+
+exec /usr/bin/dart \
+     ${DBG_OPTION} \
+     --enable-vm-service:8181/0.0.0.0 \
+     ${DART_VM_OPTIONS} \
+     bin/server.dart
